6e066be21611a2e04a7e6dd515ad2777458b8100
zepeda <jovanny.zepeda@corb.mx>
2015-02-18 23:19:20 -0600

    modales de contacto para mascotas

--- wp-content/themes/Lacalma/jupiter/single.php
+++ wp-content/themes/Lacalma/jupiter/single.php
@@ -44,7 +44,9 @@ get_header();
 						echo "<p><i class='fa fa-info-circle'></i> Información Adicional: ". get_the_content() ."</p>";
 					echo "</div>";
 					echo "<hr class='mascotas_adoptivas'>";
-					echo "<p class = 'center'><i class='fa fa-users'></i>Enviar mensaje...</p>";
+					echo "<a href = '#contacto'><button class = 'single_add_to_cart_button shop-skin-btn shop-flat-btn alt mensaje_adopta'><i class='fa fa-users'> Enviar mensaje...</i></button></a>";
+					if($encuentro == 'Adopcion')
+						echo "<a href = '#adopcion'><button class = 'single_add_to_cart_button shop-skin-btn shop-flat-btn alt mensaje_adopta margin'><i class='fa fa-users'> Adoptame</i></button></a>";
 				}
 				?>
 				<?php if($precio!="")
@@ -76,5 +78,46 @@ get_header();
 		?>
 	</div>
 </div>
-
+<div id="contacto" class="modalmask">
+    <div class="modalbox movedown">
+        <a href="#close" title="Close" class="close"><i class="fa fa-times"></i></a>
+        <h4 class="center">Mensaje de contacto</h4>
+        <form method="post">
+        	<label for = "nombre"><i class="fa fa-smile-o"></i> Nombre: </label>
+        	<input type = "text" placeholder = "Nombre" id = "nombre" name = "nombre"/>
+        	<hr class="limpia">
+        	<label for = "correo"><i class="fa fa-at"></i> Correo: </label>
+        	<input type = "email" placeholder = "Correo electrónico" id = "correo" name = "correo"/>
+        	<hr class="limpia">
+        	<label for = "numero"><i class="fa fa-mobile"></i> Tel: </label>
+        	<input type = "text" placeholder = "Teléfono de contacto" id = "numero" name = "numero"/>
+        	<hr class="limpia">
+        	<label for = "Mensaje"><i class="fa fa-envelope"></i> Mensaje: </label>
+        	<textarea id = "Mensaje" placeholder = "Introduce tu mensaje aquí..." rows = "4" cols = "50" name = "content"></textarea>
+        	<hr class="limpia">
+        	<input type = "submit" value="Enviar" class="enviar" />
+        </form>
+    </div>
+</div>
+<div id="adopcion" class="modalmask">
+    <div class="modalbox movedown">
+        <a href="#close" title="Close" class="close"><i class="fa fa-times"></i></a>
+        <h4 class="center">Mensaje de Adopción</h4>
+        <form method="post">
+        	<label for = "nombre"><i class="fa fa-smile-o"></i> Nombre: </label>
+        	<input type = "text" placeholder = "Nombre" id = "nombre" name = "nombre"/>
+        	<hr class="limpia">
+        	<label for = "correo"><i class="fa fa-at"></i> Correo: </label>
+        	<input type = "email" placeholder = "Correo electrónico" id = "correo" name = "correo"/>
+        	<hr class="limpia">
+        	<label for = "numero"><i class="fa fa-mobile"></i> Tel: </label>
+        	<input type = "text" placeholder = "Teléfono de contacto" id = "numero" name = "numero"/>
+        	<hr class="limpia">
+        	<label for = "Mensaje"><i class="fa fa-envelope"></i> Mensaje: </label>
+        	<textarea id = "Mensaje" placeholder = "Introduce tu mensaje aquí..." rows = "4" cols = "50" name = "content"></textarea>
+        	<hr class="limpia">
+        	<input type = "submit" value="Enviar" class="enviar" />
+        </form>
+    </div>
+</div>
 <?php get_footer(); ?>
